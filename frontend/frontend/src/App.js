a915ad479df054373de7bdbf394f645048f35c31
The-TUSK <marrubio07@gmail.com>
2024-02-22 05:10:19 -0800

    adding react frontend

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cities from "./components/Cities";
+import Read from "./components/Read";
+import Add from "./components/Add";
+import Update from "./components/Update";
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Cities />} />
+          <Route path="/read/:cityName" element={<Read />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update/:cityName" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
